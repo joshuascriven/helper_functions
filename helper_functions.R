168137b65a3044b045cbe7d2abf0c39107965259
Joshua Scriven <38514876+joshuascriven@users.noreply.github.com>
2021-06-04 13:57:18 -0400

    Update helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -18,12 +18,48 @@ print2 <- function(...){
 
 # (install and) load installed packages
 packageloader <- function(...){
-  lapply(
-  ...,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-  )}
+  lapply(...,
+         FUN = function(x) {
+           if (!require(x, character.only = TRUE)) {
+             install.packages(x, dependencies = TRUE)
+             library(x, character.only = TRUE)
+           }})}
+
+#load this rmd YAML header with commented out options to the clipboard
+writeClipboard('---
+title: "LOC Data Request 2"
+author: "Joshua Scriven"
+date: "6/3/2021"
+# output: ioslides_presentation
+# output: word_document
+
+#   # requires install.packages("revealjs")
+  # output:
+#   revealjs::revealjs_presentation: 
+#     theme: solarized
+#     highlight: tango
+#     transition: slide
+
+#Requires install.packages("rmdformats ")
+# output: rmdformats::readthedown
+# output: rmdformats::html_clean
+
+# output:
+#   slidy_presentation:
+#     # theme: darkly
+#     # theme: lumen # good
+#     # theme: readable
+#     theme: cerulean # good
+    # theme: journal
+    # theme: flatly
+    # theme: spacelab
+    # theme: united
+    # theme: cosmo #good
+    # theme: paper
+    # theme: sandstone
+    # theme: simplex
+    # theme: yeti # good
+# output: slidy_presentation
+output: html_notebook
+# output: powerpoint_presentation
+---')
