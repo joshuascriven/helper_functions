de75244aa71d01d4e3db58f8f58261b1cbe5897c
Joshua Scriven <38514876+joshuascriven@users.noreply.github.com>
2021-12-06 10:08:36 -0500

    Update helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -31,6 +31,19 @@ seqlast <- function (from, to, by)
   }
 }
 
+# for custom margin line locations 
+line2user <- function(line, side) {
+  lh <- par('cin')[2] * par('cex') * par('lheight')
+  x_off <- diff(grconvertX(c(0, lh), 'inches', 'npc'))
+  y_off <- diff(grconvertY(c(0, lh), 'inches', 'npc'))
+  switch(side,
+         `1` = grconvertY(-line * y_off, 'npc', 'user'),
+         `2` = grconvertX(-line * x_off, 'npc', 'user'),
+         `3` = grconvertY(1 + line * y_off, 'npc', 'user'),
+         `4` = grconvertX(1 + line * x_off, 'npc', 'user'),
+         stop("Side must be 1, 2, 3, or 4", call.=FALSE))
+}
+
 # formula paster
 formfunc <- function(dv, ivs,int){
   if(missing(int)) {
